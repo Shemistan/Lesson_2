da1651dcfeb174eac0d770f4976c1ce13505f68b
Пользователь <rus_280198@mail.ru>
2023-07-31 22:38:02 +0500

    lesson_2

--- go.mod
+++ go.mod
@@ -1,3 +1,1 @@
 module github.com/Shemistan/Lesson_2
-
-go 1.21rc3
--- main.go
+++ main.go
@@ -6,7 +6,21 @@ const (
 	exit = "exit"
 	auth = "auth"
 	reg  = "reg"
+	help = "help"
+	add_product = "add_product"
+	order = "order"
+
 )
+
+func checkExist(List []string, item string) bool {
+	for _, v := range List {
+		if v == item {
+			return true
+		}
+	}
+
+	return false
+}
 
 func main() {
 	var command string
@@ -15,34 +29,57 @@ func main() {
 
 	_ = productList
 	for command != exit {
-		fmt.Println("Введите команду") // Сделать красивый вывод, вывести список команд на этом шаге
+		fmt.Println("Введите команду. Введите help для получения списка команд.")
 		fmt.Scan(&command)
 
 		switch command {
-		case exit:
-			break
+		case help:
+			fmt.Println("reg - Регистрация пользователя.")
+			fmt.Println("auth - Авторизация")
+			fmt.Println("exit - Закончить работу.")
+			fmt.Println("add_product - Добавить продукт в корзину.")
+			fmt.Println("order - Заказать товары из корзины.")
 		case reg:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
-			fmt.Scan(&command) // Сделать так, что бы выводил сообщение, если пользователь уже существует
-			userList = append(userList, command)
-
-			message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
-			fmt.Println(message)
-
-			fmt.Println(userList)
+			fmt.Println("Введите логин и пароль в формате login_password:")
+			fmt.Scan(&command) 
+			if checkExist(userList, command) == true {
+				fmt.Println("Такой пользователь уже существует")
+			} else {
+				userList = append(userList, command)
+				message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
+				fmt.Println(message)
+			}
 		case auth:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
+			fmt.Println("Введите логин и пароль в таком виде login_password")
 			fmt.Scan(&command)
 
-			for _, v := range userList {
-				if v == command {
-					fmt.Println("Добро пожаловать в магази")
-
-				} else {
-					fmt.Println("Вы не зарегистрированны")
-				}
+			if checkExist(userList, command) == true {
+				fmt.Println("Добро пожаловать в магазин")
+			} else {
+				fmt.Println("Вы не зарегистрированы")
 
 			}
+		case add_product:
+			fmt.Println("Введите наименование товара:")
+			fmt.Scan(&command)
+
+			productList = append(productList, command)
+			message := fmt.Sprintf("Товар %s успешно добавлен", command)
+			fmt.Println(message)
+		case order:
+			if len(productList) == 0 {
+				fmt.Println("Корзина пуста")
+			} else {
+				fmt.Println("Спасибо за заказ. Ваш список товаров:")
+				for _, v := range productList {
+					fmt.Println(v)
+				}
+			}
+			productList = productList[:0]
+		case exit:
+			break
+		default:
+			fmt.Println("Введена неправильная команда.")
 		}
 	}
 }
