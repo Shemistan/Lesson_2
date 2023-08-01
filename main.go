8fda9cfa9100b3c11319dc0f098c5e67cbb1bcce
PAVEL ZEMSKOV <pzemskov@gmail.com>
2023-08-01 20:27:57 +0500

    test_add_product

--- main.go
+++ main.go
@@ -3,40 +3,50 @@ package main
 import "fmt"
 
 const (
-	exit = "exit"
-	auth = "auth"
-	reg  = "reg"
+	exit        = "exit"
+	auth        = "auth"
+	reg         = "reg"
+	add_product = "add_product"
+	order       = "order"
 )
 
 func main() {
 	var command string
-	userList := []string{"user1_password1", "user1_password1"}
+	userList := []string{"user1_password1", "user2_password2"}
 	productList := make([]string, 0, 10)
 
 	_ = productList
 	for command != exit {
-		fmt.Println("Введите команду") // Сделать красивый вывод, вывести список команд на этом шаге
+		fmt.Println("Введите команду, одну из exit/auth/reg/add") // Сделать красивый вывод, вывести список команд на этом шаге
 		fmt.Scan(&command)
 
 		switch command {
 		case exit:
 			break
 		case reg:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
+			fmt.Println("Введите логин и пароль в таком виде login_password")
 			fmt.Scan(&command) // Сделать так, что бы выводил сообщение, если пользователь уже существует
 			userList = append(userList, command)
 
 			message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
 			fmt.Println(message)
 
-			fmt.Println(userList)
+			//fmt.Println(userList)
 		case auth:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
+			fmt.Println("Введите логин и пароль в таком виде login_password")
 			fmt.Scan(&command)
 
 			for _, v := range userList {
 				if v == command {
 					fmt.Println("Добро пожаловать в магази")
+					//если авторизованы - добавляем товары
+					fmt.Scan(&command)
+					if command == "add_product" {
+						fmt.Println("Какой товар хотите добавить?")
+						fmt.Scan(&command)
+						_ = append(_, command)
+						fmt.Println("Следующие товары добавлены в корзину", _)
+					}
 
 				} else {
 					fmt.Println("Вы не зарегистрированны")
