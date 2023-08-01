aabb3dbaae5fbd34adb512a6520705fbb80e508f
Faxa2209 <faxa2209@gmail.com>
2023-08-01 10:40:51 +0500

    Updated menu output, reg and auth cases

--- main.go
+++ main.go
@@ -1,48 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const (
-	exit = "exit"
-	auth = "auth"
-	reg  = "reg"
+	exit        = "exit"
+	auth        = "auth"
+	reg         = "reg"
 )
+
+var menuInfo = map[string]string{
+	"reg":         "регистрация нового профиля",
+	"auth":        "авторизация в существующий профиль",
+	"add_product": "добавить продукт в корзину",
+	"order":       "создаёт чек и очищает корзину",
+	"exit":        "выход из приложения",
+}
+
+func menuOutput() {
+	helloMsgUpper := `|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|          Добро пожаловать в Магазин        |`
+	helloMsgLower := `|                           version by Faxa  |
+|____________________________________________|`
+	fmt.Printf("\033[1m%s\033[0m\n%s\n\n", helloMsgUpper, helloMsgLower)
+	fmt.Println("╭─ \033[1mДоступные команды:\033[0m")
+	for k, v := range menuInfo {
+		// *: Команды будет выводиться рандомно т.к. это Map
+		fmt.Printf("├─ \033[1m%s\033[0m - %s\n", k, v)
+	}
+	fmt.Print("Введите команду: ")
+}
 
 func main() {
 	var command string
-	userList := []string{"user1_password1", "user1_password1"}
+	var userData string
+	userList := make(map[string]string)
 	productList := make([]string, 0, 10)
 
 	_ = productList
 	for command != exit {
-		fmt.Println("Введите команду") // Сделать красивый вывод, вывести список команд на этом шаге
+		menuOutput()
 		fmt.Scan(&command)
 
 		switch command {
 		case exit:
-			break
 		case reg:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
-			fmt.Scan(&command) // Сделать так, что бы выводил сообщение, если пользователь уже существует
-			userList = append(userList, command)
+			fmt.Println("Введите логин и пароль в таком виде login_password")
+			fmt.Scan(&userData)
 
-			message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
+			if !strings.Contains(userData, "_") {
+				fmt.Println("\n\x1b[31mНекорректный ввод. Ожидается формат 'login_password'\x1b[0m\n")
+				break
+			}
+
+			loginPass := strings.Split(userData, "_")
+			login := loginPass[0]
+			password := loginPass[1]
+
+			if _, ok := userList[login]; ok {
+				fmt.Println("\n\x1b[33mЭтот пользователь уже существует\x1b[30m\n")
+				break
+			}
+			userList[login] = password
+			message := fmt.Sprintf("\n\x1b[32mПользователь %s успешно добавлен\x1b[0m", login)
 			fmt.Println(message)
 
-			fmt.Println(userList)
+			fmt.Println(userList, "\n")
 		case auth:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
-			fmt.Scan(&command)
+			fmt.Println("Введите логин и пароль в таком виде login_password")
+			fmt.Scan(&userData)
 
-			for _, v := range userList {
-				if v == command {
-					fmt.Println("Добро пожаловать в магази")
-
-				} else {
-					fmt.Println("Вы не зарегистрированны")
-				}
-
+			if !strings.Contains(userData, "_") {
+				fmt.Println("\n\x1b[31mНекорректный ввод. Ожидается формат 'login_password'\x1b[0m\n")
+				break
 			}
+
+			loginPass := strings.Split(userData, "_")
+			login := loginPass[0]
+			fmt.Println()
+			if _, ok := userList[login]; ok {
+				fmt.Println("\n\x1b[32mДобро пожаловать в магазин\x1b[0m\n")
+				break
+			}
+			fmt.Println("\x1b[33mВы ещё не зарегистрированы\x1b[0m\n")
+		default:
+			fmt.Println("\n\x1b[31mТакой команды не существует\x1b[0m\n")
 		}
 	}
 }
