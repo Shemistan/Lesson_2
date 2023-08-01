fbd9f2a3b6b89dafe7aa6cbb79771beb03f37091
Faxa2209 <faxa2209@gmail.com>
2023-08-01 11:19:15 +0500

    Implemented "add_product" and "order" cases

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ const (
 	exit        = "exit"
 	auth        = "auth"
 	reg         = "reg"
+	order       = "order"
+	add_product = "add_product"
 )
 
 var menuInfo = map[string]string{
@@ -36,10 +38,10 @@ func menuOutput() {
 func main() {
 	var command string
 	var userData string
+	var currentUser string
 	userList := make(map[string]string)
 	productList := make([]string, 0, 10)
 
-	_ = productList
 	for command != exit {
 		menuOutput()
 		fmt.Scan(&command)
@@ -80,17 +82,42 @@ func main() {
 			loginPass := strings.Split(userData, "_")
 			login := loginPass[0]
 			fmt.Println()
+
 			if _, ok := userList[login]; ok {
 				fmt.Println("\n\x1b[32mДобро пожаловать в магазин\x1b[0m\n")
+				currentUser = login
 				break
 			}
 			fmt.Println("\x1b[33mВы ещё не зарегистрированы\x1b[0m\n")
+		case add_product:
+			if currentUser == "" {
+				fmt.Println("\n\x1b[33mПользователь не авторизован\x1b[0m\n")
+				break
+			}
+			fmt.Print("Введите продукт: ")
+			var product string
+			fmt.Scan(&product)
+
+			productList = append(productList, product)
+			fmt.Printf("\n\x1b[32m%s\x1b[0m был добавлен в корзину\n\n", product)
+		case order:
+			if currentUser == "" {
+				fmt.Println("\n\x1b[33mПользователь не авторизован\x1b[0m\n")
+				break
+			}
+			if len(productList) <= 0 {
+				fmt.Println("\n\x1b[33mКорзина пуста\x1b[0m\n")
+				break
+			}
+			fmt.Println()
+			fmt.Printf("Чек пользователя - %s\n", currentUser)
+			for i := range productList {
+				fmt.Println("* " + productList[i])
+			}
+			fmt.Println()
+			productList = []string{}
 		default:
 			fmt.Println("\n\x1b[31mТакой команды не существует\x1b[0m\n")
 		}
 	}
 }
-
-// Реализовать следующие АПИ
-// add_product - добавить продукт который вводиться с консоли в productList
-// order - выводит сообщение что вы купили и очищает корзину
