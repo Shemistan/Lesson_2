4836c6e8aee9b1eab0f5e27e641c0968998676c5
yetwink <aynatdinovvs@gmail.com>
2023-08-03 01:50:03 +0500

    Lesson 2 Homework, Ermurat

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 const (
-	exit = "exit"
-	auth = "auth"
-	reg  = "reg"
+	exit        = "exit"
+	auth        = "auth"
+	reg         = "reg"
+	add_product = "add_product"
+	order       = "order"
 )
 
 func main() {
@@ -13,36 +15,63 @@ func main() {
 	userList := []string{"user1_password1", "user1_password1"}
 	productList := make([]string, 0, 10)
 
-	_ = productList
+	var auth_stat bool
+	var reg_stat bool
+
+	fmt.Println("    Добро пожаловать в Uzum-market!")
+
 	for command != exit {
-		fmt.Println("Введите команду") // Сделать красивый вывод, вывести список команд на этом шаге
+		fmt.Println(`Используйте следующие команды: "auth", "reg", "add_product", "order", "exit"`) // Сделать красивый вывод, вывести список команд на этом шаге
 		fmt.Scan(&command)
 
 		switch command {
 		case exit:
 			break
-		case reg:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
-			fmt.Scan(&command) // Сделать так, что бы выводил сообщение, если пользователь уже существует
-			userList = append(userList, command)
 
-			message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
-			fmt.Println(message)
-
-			fmt.Println(userList)
-		case auth:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
+		case reg: // Сделать так, что бы выводил сообщение, если пользователь уже существует
+			fmt.Println("Введите логин и пароль в таком виде login_password")
 			fmt.Scan(&command)
 
 			for _, v := range userList {
-				if v == command {
-					fmt.Println("Добро пожаловать в магази")
-
-				} else {
-					fmt.Println("Вы не зарегистрированны")
+				if v == command && reg_stat == false {
+					fmt.Println("Вы уже зарегистрированы! Пользователь уже существует.")
+					reg_stat = true
 				}
-
 			}
+			if reg_stat == false {
+				userList = append(userList, command)
+				message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
+				fmt.Println(message)
+			}
+
+			fmt.Printf("Зарегистрированные аккаунты:  %q \n", userList)
+
+		case auth:
+			fmt.Println("Введите логин и пароль в таком виде login_password")
+			fmt.Scan(&command)
+			for _, v := range userList {
+				if v == command {
+					fmt.Println("    Добро пожаловать в Uzum-market! Вы авторизовались!")
+					auth_stat = true
+
+				}
+			}
+			if auth_stat == false {
+				fmt.Println("Вы не зарегистрированы!")
+			}
+
+		case add_product:
+			fmt.Println("В корзине: ", productList)
+			fmt.Println("Добавьте продукт в корзину: ")
+			fmt.Scan(&command)
+			productList = append(productList, command)
+			fmt.Printf("Продукт - '%v' добавлен! ", command)
+			fmt.Printf("%q \n", productList)
+
+		case order:
+			//fmt.Println("Поздравляю, вы купили: ")
+			fmt.Printf("Поздравляю, вы купили: %q \n", productList)
+			productList = productList[:0]
 		}
 	}
 }
