ff2c0c7d413b035a4d701d9e69dcb248086ecd8d
nasrullonurullaev5 <nasrullonurullaev5@gmail.com>
2023-08-16 13:23:51 +0500

    Homework

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Shemistan/Lesson_2
+module github.com/nasrullonurullaev5/Lesson_2
 
-go 1.21rc3
+go 1.21
--- main.go
+++ main.go
@@ -3,43 +3,67 @@ package main
 import "fmt"
 
 const (
-	exit = "exit"
-	auth = "auth"
-	reg  = "reg"
+	exit        = "exit"
+	auth        = "auth"
+	reg         = "reg"
+	add_product = "add_product"
+	order       = "order"
 )
 
 func main() {
 	var command string
-	userList := []string{"user1_password1", "user1_password1"}
+	var command1 string
+	userList := []string{"user1_password1"}
 	productList := make([]string, 0, 10)
 
 	_ = productList
 	for command != exit {
-		fmt.Println("Введите команду") // Сделать красивый вывод, вывести список команд на этом шаге
+		fmt.Println("Введите команду! \n Для авторизации - 'auth' \n Для регистрации - 'reg' \n Для выхода - 'exit' ")
 		fmt.Scan(&command)
 
 		switch command {
 		case exit:
 			break
 		case reg:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
+			fmt.Println("Введите логин и пароль в таком виде login_password")
 			fmt.Scan(&command) // Сделать так, что бы выводил сообщение, если пользователь уже существует
+			for _, user := range userList {
+				if user == command {
+					fmt.Println("Пользователь уже существует")
+					return
+				}
+			}
 			userList = append(userList, command)
 
 			message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
 			fmt.Println(message)
 
-			fmt.Println(userList)
 		case auth:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
+			fmt.Println("Введите логин и пароль в таком виде login_password")
 			fmt.Scan(&command)
 
 			for _, v := range userList {
 				if v == command {
-					fmt.Println("Добро пожаловать в магази")
+					fmt.Println("Добро пожаловать в магазин!")
+					fmt.Println("Введите команду \n Для добавление продукта - add_product \n Для вывода корзины - order")
+					switch command1 {
+					case add_product:
+						fmt.Println("Введите продукт")
+						fmt.Scan(&command1)
+						productList = append(productList, command1)
+						message := fmt.Sprintf("Продукт %s успешно добавлен", command1)
+						fmt.Println(message)
 
+					case order:
+						message := fmt.Sprintf("Вы купили %s", productList)
+						fmt.Println(message)
+						fmt.Println("Корзина очищена")
+						fmt.Println(productList)
+						break
+					}
 				} else {
 					fmt.Println("Вы не зарегистрированны")
+					break
 				}
 
 			}
