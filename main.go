203afc40a6baa889c471a03aefed3b3e7f45d2ab
Bekzod Askarov <44816035+askbekzod@users.noreply.github.com>
2023-08-21 17:21:01 +0500

    add_product, order options

--- main.go
+++ main.go
@@ -3,50 +3,94 @@ package main
 import "fmt"
 
 const (
-	exit = "exit"
-	auth = "auth"
-	reg  = "reg"
+	exit       = "exit"
+	auth       = "auth"
+	reg        = "reg"
+	addProduct = "add_product"
+	order      = "order"
 )
 
 func main() {
 	var command string
-	userList := []string{"user1_password1", "user1_password1"}
+	userList := []string{"user1_password1", "user2_password2"} // Примеры пользователей
 	productList := make([]string, 0, 10)
 
-	_ = productList
+	authenticated := false
+
 	for command != exit {
-		fmt.Println("Введите команду") // Сделать красивый вывод, вывести список команд на этом шаге
+		fmt.Println("Выберите команду:")
+		fmt.Println("1. " + exit + ": Выход из программы")
+		fmt.Println("2. " + reg + ": Регистрация нового пользователя")
+		fmt.Println("3. " + auth + ": Авторизация пользователя")
+		if authenticated {
+			fmt.Println("4. " + addProduct + ": добавить продукт в корзину")
+			fmt.Println("5. " + order + ": показать корзину и оформить заказ")
+		}
+		fmt.Print("Введите номер команды: ")
 		fmt.Scan(&command)
 
 		switch command {
-		case exit:
-			break
-		case reg:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
-			fmt.Scan(&command) // Сделать так, что бы выводил сообщение, если пользователь уже существует
-			userList = append(userList, command)
-
-			message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
-			fmt.Println(message)
-
-			fmt.Println(userList)
-		case auth:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
+		case "1":
+			fmt.Println("Программа завершена.")
+			return
+		case "2":
+			fmt.Print("Введите логин и пароль в формате login_password: ")
 			fmt.Scan(&command)
-
-			for _, v := range userList {
-				if v == command {
-					fmt.Println("Добро пожаловать в магази")
-
-				} else {
-					fmt.Println("Вы не зарегистрированны")
-				}
-
+			// Проверка на существующего пользователя
+			if userExists(userList, command) {
+				fmt.Println("Пользователь уже существует.")
+			} else {
+				userList = append(userList, command)
+				fmt.Printf("Пользователь %s успешно добавлен.\n", command)
 			}
+		case "3":
+			fmt.Print("Введите логин и пароль в формате login_password: ")
+			fmt.Scan(&command)
+			// Поиск пользователя в списке
+			if userExists(userList, command) {
+				fmt.Println("Добро пожаловать в магазин!")
+				authenticated = true
+			} else {
+				fmt.Println("Вы не зарегистрированы.")
+				authenticated = false
+			}
+		case "4":
+			if authenticated {
+				fmt.Print("Введите название продукта: ")
+				fmt.Scan(&command)
+				productList = append(productList, command)
+				fmt.Printf("Продукт %s успешно добавлен в корзину.\n", command)
+			} else {
+				fmt.Println("Вы не авторизованы.")
+			}
+
+		case "5":
+			if authenticated {
+				if len(productList) == 0 {
+					fmt.Println("Ваша корзина пуста.")
+				} else {
+					fmt.Println("Ваша корзина:")
+					for _, v := range productList {
+						fmt.Println(v)
+					}
+					fmt.Println("Заказ успешно оформлен.")
+					productList = make([]string, 0, 10)
+				}
+			} else {
+				fmt.Println("Вы не авторизованы.")
+			}
+
+		default:
+			fmt.Println("Неверная команда. Пожалуйста, выберите команду из списка.")
 		}
 	}
 }
 
-// Реализовать следующие АПИ
-// add_product - добавить продукт который вводиться с консоли в productList
-// order - выводит сообщение что вы купили и очищает корзину
+func userExists(userList []string, user string) bool {
+	for _, v := range userList {
+		if v == user {
+			return true
+		}
+	}
+	return false
+}
