36cc02ff693c053efbda4bb9b9b6786430040d4b
shahTeam <shahzodasturchi@gmail.com>
2023-09-28 15:17:41 +0500

    sheet code

--- main.go
+++ main.go
@@ -2,48 +2,56 @@ package main
 
 import "fmt"
 
-const (
-	exit = "exit"
-	auth = "auth"
-	reg  = "reg"
-)
-
 func main() {
-	var command string
-	userList := []string{"user1_password1", "user1_password1"}
-	productList := make([]string, 0, 10)
-
-	_ = productList
-	for command != exit {
-		fmt.Println("Введите команду") // Сделать красивый вывод, вывести список команд на этом шаге
-		fmt.Scan(&command)
-
-		switch command {
-		case exit:
-			break
-		case reg:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
-			fmt.Scan(&command) // Сделать так, что бы выводил сообщение, если пользователь уже существует
-			userList = append(userList, command)
-
-			message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
-			fmt.Println(message)
-
-			fmt.Println(userList)
-		case auth:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
-			fmt.Scan(&command)
-
-			for _, v := range userList {
-				if v == command {
-					fmt.Println("Добро пожаловать в магази")
-
-				} else {
-					fmt.Println("Вы не зарегистрированны")
-				}
-
-			}
+	var name string
+	var lastName string
+	var number int
+	var number1 int
+	var exit string
+	var yes string
+	products := []string{"apple", "lemon", "bread", "meat", "milk"}
+	cost := []int{200, 300, 400 ,500, 600}
+	fmt.Println("Welcom to my Shop!!!")
+	fmt.Println("Please enter your name!")
+	fmt.Scan(&name)
+	fmt.Println("Please enter your lastname!")
+	fmt.Scan(&lastName)
+	fmt.Println("You Are seller or customer")
+	fmt.Println("1. Customer")
+	fmt.Println("2. Seller")
+	fmt.Scan(&number)
+	if number == 1 {
+		fmt.Println("List of products!")
+		for i, val := range products{
+			fmt.Println(i+1,".", val)
 		}
+		fmt.Println("List of costs product!")
+		for i, value := range cost{
+			fmt.Println(i+1, ".", value)
+		} 
+		productBox := make([]string, 0)
+	fmt.Println("what you need!", "If you need any product, enter the product number!")
+	fmt.Println("if yo don't any order product pleas write exit")
+	fmt.Scan(&number1)
+	if exit == "exit"{
+		fmt.Println("goodbye!") 
+	}else if number1 == 1{
+		productBox = append(productBox, products[0])
+	}else if number1 == 2 {
+		productBox =append(productBox, products[1])
+	}else if number1 == 3 {
+		productBox = append(productBox, products[2])
+	}else if number1 == 4 {
+		productBox = append(productBox, products[3])
+	}
+	fmt.Println("if you want see your product box enter yes")
+	fmt.Scan(&yes)
+	if yes == "yes" {
+		fmt.Println(productBox)
+		fmt.Println("Bill",cost[number1])
+	}
+	}else if number == 2 {
+		fmt.Println("Have good day colleague")
 	}
 }
 
